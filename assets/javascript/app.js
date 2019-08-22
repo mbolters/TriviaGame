3c1fe000218b808e69642a0acee2fd4592242ad3
mbolters <mariematisans@gmail.com>
2019-08-21 21:02:39 -0500

    add questions/answers objects

---
+++ assets/javascript/app.js
@@ -0,0 +1,30 @@
+// objects for questions and answers in an array
+
+let questions = [
+    {
+    question: "question 1",
+    answers: ["answer1", "answer2", "answer3", "answer4"],
+    correctAnswer: 2
+    },
+    {
+    question: "question 2",
+    answers: ["answer1", "answer2", "answer3", "answer4"],
+    correctAnswer: 3
+    },
+    {
+    question: "question 3",
+    answers: ["answer1", "answer2", "answer3", "answer4"],
+    correctAnswer: 1
+    },
+    {
+    question: "question 4",
+    answers: ["answer1", "answer2", "answer3", "answer4"],
+    correctAnswer: 4
+    },
+    {
+    question: "question 5",
+    answers: ["answer1", "answer2", "answer3", "answer4"],
+    correctAnswer: 2   
+    }
+];
+
